082f482812c51ba581305e075e1d4b09d93083a9
maidominh7121993-source <maidominh7121993@gmail.com>
2026-03-11 04:52:13 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:40,fontFamily:"Arial"}}>
+      <h1>Hệ thống quản lý F0 F1 F2</h1>
+      <p>Website đã chạy thành công</p>
+    </div>
+  )
+}
